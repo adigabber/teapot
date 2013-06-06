a64eccd00349ae551fd12d78228daaf921d0a194
Elad Zoldan <eladzo@etoro.com>
2013-06-06 19:58:38 +0300

    Fix the html popup

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ var port = chrome.runtime.connect({
 	name: "fromPOtoBG"
 });
 
+
 port.postMessage({
 	funcName: firsttime_ui,
 	pack: "AllCamp"
@@ -12,11 +13,12 @@ port.postMessage({
 port.onMessage.addListener(function(msg) {
 
 	if (msg.funcName == firsttime_ui) {
-		var body = document.getElementsByTagName('body')[0];
+       
+		var body = $('.span10')[0];
 		var tbl = document.createElement('table');
-		tbl.style.width = '100';
-		tbl.setAttribute('border', '1');
-		var tbdy = document.createElement('tbody');
+		$(tbl).addClass("table table-striped");
+		 var tbdy = document.createElement('tbody');
+		
 		for (campCounter = 0; campCounter < msg.pack.length; campCounter++) {
 			var tr = document.createElement('tr');
 			var td = document.createElement('td');
@@ -27,33 +29,42 @@ port.onMessage.addListener(function(msg) {
 		tbl.appendChild(tbdy);
 		body.appendChild(tbl);
 
-		var buttonPaused = document.createElement('input');
-		buttonPaused.setAttribute('type', 'button');
-		buttonPaused.setAttribute('name', 'Paused');
-		buttonPaused.setAttribute('value', 'Paused All Campaigns');
-		buttonPaused.onclick = function() {
-			port.postMessage({
-				funcName: "Paused",
-				pack: "allcamp"
-			});
-		};
-		body.appendChild(buttonPaused);
 
-		var buttonDupTw = document.createElement('input');
-		buttonDupTw.setAttribute('type', 'button');
-		buttonDupTw.setAttribute('name', 'DupTw');
-		buttonDupTw.setAttribute('value', 'Duplicate All Campaigns tweets');
-		buttonDupTw.onclick = function() {
-			port.postMessage({
-				funcName: "DupTw",
-				pack: msg.pack
 
-			});
-		};
-		body.appendChild(buttonDupTw);
+		$('.btn').click(function() {
+			alert(this.name);
+			//	port.postMessage({
+			// 		funcName: this.name,
+			// 		pack: msg.pack //TODO:  I will create the checkbooks I will take only the campaigns who clicked..
+		});
 	} else {
 		document.write(msg.pack);
 	}
 });
 
 
+
+// var buttonPaused = document.createElement('input');
+// buttonPaused.setAttribute('type', 'button');
+// buttonPaused.setAttribute('name', 'Paused');
+// buttonPaused.setAttribute('value', 'Paused All Campaigns');
+// buttonPaused.onclick = function() {
+// 	port.postMessage({
+// 		funcName: "Paused",
+// 		pack: "allcamp"
+// 	});
+// };
+// body.appendChild(buttonPaused);
+
+// var buttonDupTw = document.createElement('input');
+// buttonDupTw.setAttribute('type', 'button');
+// buttonDupTw.setAttribute('name', 'DupTw');
+// buttonDupTw.setAttribute('value', 'Duplicate All Campaigns tweets');
+// buttonDupTw.onclick = function() {
+// 	port.postMessage({
+// 		funcName: "DupTw",
+// 		pack: msg.pack
+
+// 	});
+// };
+// body.appendChild(buttonDupTw);
