8497e88444a250fc00d6e03594f1c10b5103e4e2
Elad Zoldan <eladzo@etoro.com>
2013-06-09 17:47:11 +0300

    Headline to UI table..

--- popup.js
+++ popup.js
@@ -1,8 +1,15 @@
 var firsttime_ui = "UI"
+var headsOfColumnsInTable = ["#", "Campaign name"];
+var columnFunc = {
+	"#": row_num,
+	"Campaign name": camp_name
+}
+
 
 var port = chrome.runtime.connect({
 	name: "fromPOtoBG"
 });
+
 
 
 port.postMessage({
@@ -13,17 +20,32 @@ port.postMessage({
 port.onMessage.addListener(function(msg) {
 
 	if (msg.funcName == firsttime_ui) {
-       
+
 		var body = $('.span10')[0];
 		var tbl = document.createElement('table');
 		$(tbl).addClass("table table-striped");
-		 var tbdy = document.createElement('tbody');
 		
+		var thead = document.createElement('thead');
+        var trhead =document.createElement('tr');
+        for (headCount = 0; headCount < headsOfColumnsInTable.length; headCount++) {
+        var th = document.createElement('th');
+        var headText = document.createTextNode(headsOfColumnsInTable[headCount]);
+        th.appendChild(headText);
+        trhead.appendChild(th);
+        }
+        thead.appendChild(trhead);
+        tbl.appendChild(thead);
+
+
+		var tbdy = document.createElement('tbody');
 		for (campCounter = 0; campCounter < msg.pack.length; campCounter++) {
 			var tr = document.createElement('tr');
-			var td = document.createElement('td');
-			td.appendChild(document.createTextNode(msg.pack[campCounter].camp));
-			tr.appendChild(td)
+			for (colCount = 0; colCount < headsOfColumnsInTable.length; colCount++) {
+				var td = document.createElement('td');
+				var rowData = columnFunc[headsOfColumnsInTable[colCount]](campCounter, msg.pack);
+				td.appendChild(rowData);
+				tr.appendChild(td)
+			}
 			tbdy.appendChild(tr);
 		}
 		tbl.appendChild(tbdy);
@@ -32,39 +54,24 @@ port.onMessage.addListener(function(msg) {
 
 
 		$('.btn').click(function() {
-			alert(this.name);
-			//	port.postMessage({
-			// 		funcName: this.name,
-			// 		pack: msg.pack //TODO:  I will create the checkbooks I will take only the campaigns who clicked..
+			// alert(this.name);
+				port.postMessage({
+					funcName: this.name,
+					pack: msg.pack //TODO:  I will create the checkbooks I will take only the campaigns who clicked..
+			});
 		});
 	} else {
-		document.write(msg.pack);
+		var body = $('.span10')[0];
+		respondeFromFunc = document.createTextNode(msg.pack);
+		body.appendChild(respondeFromFunc);
 	}
 });
 
+function camp_name(counter, msgPack) {
+	return document.createTextNode(msgPack[counter].camp);
+}
 
 
-// var buttonPaused = document.createElement('input');
-// buttonPaused.setAttribute('type', 'button');
-// buttonPaused.setAttribute('name', 'Paused');
-// buttonPaused.setAttribute('value', 'Paused All Campaigns');
-// buttonPaused.onclick = function() {
-// 	port.postMessage({
-// 		funcName: "Paused",
-// 		pack: "allcamp"
-// 	});
-// };
-// body.appendChild(buttonPaused);
-
-// var buttonDupTw = document.createElement('input');
-// buttonDupTw.setAttribute('type', 'button');
-// buttonDupTw.setAttribute('name', 'DupTw');
-// buttonDupTw.setAttribute('value', 'Duplicate All Campaigns tweets');
-// buttonDupTw.onclick = function() {
-// 	port.postMessage({
-// 		funcName: "DupTw",
-// 		pack: msg.pack
-
-// 	});
-// };
-// body.appendChild(buttonDupTw);
+function row_num(counter, msgPack) {
+	return document.createTextNode(counter + 1);
+}
