3a069441b14ec2a9a6063b785ce6ff75449ce156
Elad Zoldan <eladzo@etoro.com>
2013-06-17 18:06:37 +0300

    Send all  camps array.

--- popup.js
+++ popup.js
@@ -57,8 +57,8 @@ port.onMessage.addListener(function(msg) {
 			// alert(this.name); 
 				port.postMessage({
 					funcName: this.name,
-					pack: [msg.pack[0],msg.pack[1]]
-					// pack: msg.pack //TODO:  I will create the checkbooks I will take only the campaigns who clicked..
+					// pack: [msg.pack[0],msg.pack[1]]
+					pack: msg.pack //TODO:  I will create the checkbooks I will take only the campaigns who clicked..
 			});
 		});
 	} else {
